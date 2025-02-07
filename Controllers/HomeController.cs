e1879c1c6ad426b0c549715f22e0c1c14c6004be
D RONZ <somesh.chauhan.255@gmail.com>
2025-02-07 13:54:40 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using TaskSolving.Models;
+
+namespace TaskSolving.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        HttpClient client = new HttpClient();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            List<Hotel> li = new List<Hotel>();
+
+
+            client.BaseAddress = new Uri("https://localhost:44309/api/HotelApi");
+
+            var response = client.GetAsync("Home");
+
+            response.Wait();
+
+            var test = response.Result;
+
+            if(test.IsSuccessStatusCode)
+            {
+                var res = test.Content.ReadAsAsync<List<Hotel>>();
+
+                res.Wait();
+
+                li = res.Result;
+
+            }
+
+
+
+            return View(li);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Hotel h)
+        {
+            client.BaseAddress = new Uri("https://localhost:44309/api/HotelApi");
+
+            var response = client.PostAsJsonAsync("HotelApi", h);
+
+            response.Wait();
+
+            var test = response.Result;
+
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/HotelApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskSolving.Models;
+
+namespace TaskSolving.Controllers
+{
+    public class HotelApiController : ApiController
+    {
+
+        Database1Entities db = new Database1Entities();
+
+
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<Hotel> list = db.Hotels.ToList();
+
+            return Ok(list);
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertData(Hotel h)
+        {
+            db.Hotels.Add(h);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
